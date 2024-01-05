736c8eb03a7968f98e107eb7d0f6c23e97d7257b
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-05 14:39:27 +0300

    monthly challenge update

--- projects/monthly_challenges/challenges/urls.py
+++ projects/monthly_challenges/challenges/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     # making it dynamic so that its handled by one function
     # "<int:month>" helps us to filter and convert the values into numbers
     path("<int:month>", views.monthly_challenge_numbers),
-    path("<str:month>", views.monthly_challenges)
+    path("<str:month>", views.monthly_challenges),
 ]
--- projects/monthly_challenges/challenges/views.py
+++ projects/monthly_challenges/challenges/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+# entering the monthly challenges into a dictionary
+
+monthly_challengez = {
+    "jan": "learn everyday",
+    "feb": "learn a new language",
+    "mar": "pray to jah",
+    "apr": "start job hunting",
+    "may": "learn java",
+    "june": "get a new phone",
+    "july": "get shredded",
+    "aug": "celebrate event",
+    "sept": "new plans",
+    "oct": "do somethin idk",
+    "nov": "idk what to tell ya",
+    "dec": "have an s22 by then",
+}
 
 # Create your views here.
 
 
 def monthly_challenge_numbers(request, month):
-    return HttpResponse(month)
+    # converting to a list
+    months = list(monthly_challengez.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("404")
+    fmonth = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + fmonth)
 
 
 def monthly_challenges(request, month):
-    text = None
-    if month == "january":
-        text = "learn everyday"
-    elif month == "february":
-        text = "workout everyday"
-    elif month == "march":
-        text = "pray to jah"
-    else:
+    try:
+        text = monthly_challengez[month]
+    except:
         return HttpResponseNotFound("Not among the months")
 
     return HttpResponse(text)
