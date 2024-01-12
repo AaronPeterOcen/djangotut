a0f163c42143dba51a8c7976fc5cbff2fafde028
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-12 15:39:13 +0300

    new working on templates

--- projects/monthly_challenges/challenges/views.py
+++ projects/monthly_challenges/challenges/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
-    "january": "Wake up at 6pm",
-    "february": "Sleep early!",
-    "march": "Learn Django for at least 20 minutes every day!",
-    "april": "Eat no chicken for the entire month!",
+    "jan": "Wake up at 6pm",
+    "feb": "Sleep early!",
+    "mar": "Learn Django for at least 20 minutes every day!",
+    "apr": "Eat no chicken for the entire month!",
     "may": "Walk for at least 100 minutes every week!",
-    "june": "Yolo",
-    "july": "Eat no meat for the entire month!",
-    "august": "Walk for at least 20 minutes every day!",
-    "september": "Spend at least 30 minutes every day socializing",
-    "october": "Eat no fish for the week!",
-    "november": "Ride for at least 20 minutes every day!",
-    "december": "Learn German",
+    "jun": "Yolo",
+    "jul": "Eat no meat for the entire month!",
+    "aug": "Walk for at least 20 minutes every day!",
+    "sep": "Spend at least 30 minutes every day socializing",
+    "oct": "Eat no fish for the week!",
+    "nov": "Ride for at least 20 minutes every day!",
+    "dec": "Learn German",
 }
 
 # Create your views here.
@@ -39,7 +40,7 @@ def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(months):
-        return HttpResponseNotFound("Invalid month")
+        return HttpResponseNotFound("Invalid month, there are only 12 months")
 
     redirect_month = months[month - 1]
     redirect_path = reverse(
@@ -51,7 +52,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
