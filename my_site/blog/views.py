dd94f1a1867484ec543099acd100383fc625cb29
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-09 04:11:49 +0300

    update

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.shortcuts import render
 
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.png",
@@ -38,7 +38,7 @@ posts = [
         "slug": "programming-is-fun",
         "image": "coding.jpg",
         "author": "Aaron",
-        "date": date(2024, 3, 5),
+        "date": date(2024, 2, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -59,7 +59,7 @@ posts = [
         "slug": "into-the-woods",
         "image": "fore.jpg",
         "author": "Aaron",
-        "date": date(2024, 3, 5),
+        "date": date(2023, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -77,16 +77,29 @@ posts = [
     """
     }
 ]
+
+def get_date(post):
+    return post["date"]
 # from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request,"blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request,"blog/index.html", {
+        "posts": latest_posts
+    })
+
     # pass
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts":all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    id_post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": id_post
+    })
