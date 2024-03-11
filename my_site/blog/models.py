258f1b04622205cbab8274ed60116b6fb5a4fe8f
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-11 10:48:22 +0300

    updating the models with author and tag classes

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -3,6 +3,17 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+    """tag model with a caption field"""
+    caption = models.CharField(max_length=20)
+
+class Author(models.Model):
+    """Author model with the following fields"""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
 class Post(models.Model):
     """New Post Class"""
     title = models.CharField(max_length=250)
@@ -11,4 +22,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    
+    # setting up a relation with author
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
