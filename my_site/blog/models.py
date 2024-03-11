92b1a4c236a10e49cf8bf9cd719fdc8c3e517968
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-11 10:03:07 +0300

    update to the models.py file added Post classs

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,14 @@
+"""model is handling the posts for the blog"""
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Post(models.Model):
+    """New Post Class"""
+    title = models.CharField(max_length=250)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    
